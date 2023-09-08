3f0bab721e19694d3cd4f75552d2611357c52266
Pham Ngoc Duong <pnd280@gmail.com>
2023-09-08 17:48:31 +0700

    added loading screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import '@styles/App.scss';
 
+import { useEffect } from 'react';
+
 import { AppContextProvider } from '@/context/AppContext';
 import Header from '@/layouts/Header.jsx';
 import Merchs from '@/layouts/Merchs';
@@ -13,6 +15,14 @@ import Testimonials from '@layouts/Testimonials';
 import CartContextProvider from './components/CartContextProvider';
 
 const App = () => {
+  useEffect(() => {
+    const loadingSpinner = document.querySelector('.loading-screen');
+
+    if (loadingSpinner) {
+      loadingSpinner.style.display = 'none';
+    }
+  }, []);
+
   return (
     <AppContextProvider>
       <Header />
